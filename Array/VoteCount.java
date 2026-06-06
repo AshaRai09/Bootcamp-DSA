e93bfe901658f62c955c4ac288f5d62dc3ed2a61
Asha <asharai251977@gmail.com>
2026-06-06 20:40:12 +0530

    updated

---
+++ Array/VoteCount.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class VoteCount{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+       int n= sc.nextInt();
+       int arr[]= new int[n];
+       for(int i=0; i<n ; i++){
+        arr[i]= sc.nextInt();
+       }
+       Arrays.sort(arr);
+       int count=0;
+       int Threshold=2;
+       for(int i=0 ;i<n-1 ; i++){
+        if(arr[i]== arr[i+1]){
+          count++;
+          if(count>Threshold){
+            System.out.println(arr[i]);
+              return;
+          }
+        }
+          else {
+            count=1;
+          }
+        
+       }
+       System.out.println("No candidate found");
+  }
+}
