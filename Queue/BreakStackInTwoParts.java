66eb9f1506c76526c7f79bd83cf51ec5d05f33b8
Asha <asharai251977@gmail.com>
2026-06-06 17:45:40 +0530

    updated

---
+++ Queue/BreakStackInTwoParts.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class BreakStackInTwoParts{
+public static void main(String args[]){
+  Scanner sc = new scanner(System.in);
+  ArrayDeque<Integer> queue = new ArrayDeque<>();
+  int n = 
+}
+}
