1b628e1bfe106004e42c8a8f5bcf9369d61ecdfc
Asha <asharai251977@gmail.com>
2026-06-08 20:49:47 +0530

    updated

---
+++ Sorting/Sort012Number.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Sort012Number {
+    public static void sortArray(int[] arr) {
+        int count0 = 0, count1 = 0, count2 = 0;
+
+        for (int num : arr) {
+            if (num == 0) count0++;
+            else if (num == 1) count1++;
+            else count2++;
+        }
+
+        int index = 0;
+
+        while (count0-- > 0) arr[index++] = 0;
+        while (count1-- > 0) arr[index++] = 1;
+        while (count2-- > 0) arr[index++] = 2;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 1, 2, 0, 1, 0};
+
+        sortArray(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
