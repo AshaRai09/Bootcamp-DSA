ec4ab766ada0b5a545fbdaa716e1853b3d8588b5
Asha <asharai251977@gmail.com>
2026-06-11 23:32:25 +0530

    updated'

---
+++ companySpecific/swapCharracter.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class swapCharracter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        char[] arr = str.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            if (!Character.isLetterOrDigit(arr[left])) {
+                left++;
+            }
+            else if (!Character.isLetterOrDigit(arr[right])) {
+                right--;
+            }
+            else {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        System.out.println(new String(arr));
+    }
+}
