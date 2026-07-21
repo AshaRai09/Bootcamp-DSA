162ae2a80f3ea242d2d9933a5b098c41565ef1ae
Asha <asha0987rai@gmail.com>
2026-07-21 23:14:21 +0530

    'updated'

---
+++ Graph/AdjacencyList.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Graph {
+    private int V;
+    private ArrayList<ArrayList<Integer>> adj;
+
+    // Constructor
+    Graph(int V) {
+        this.V = V;
+        adj = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+    }
+
+    // Add an edge
+    void addEdge(int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u); // Remove this line for directed graph
+    }
+
+    // Display adjacency list
+    void display() {
+        System.out.println("Adjacency List:");
+
+        for (int i = 0; i < V; i++) {
+            System.out.print(i + " -> ");
+
+            for (int neighbor : adj.get(i)) {
+                System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class AdjacencyList{
+    public static void main(String[] args) {
+
+        Graph g = new Graph(5);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+
+        g.display();
+    }
+}
